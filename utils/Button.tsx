c027bab48c74a012c726ed1c1187af9c62da4143
Ronny <91857581+macarron001@users.noreply.github.com>
2022-08-23 23:41:09 +0800

    refactored Button to accept 'type' prop

--- utils/Button.tsx
+++ utils/Button.tsx
@@ -1,13 +1,20 @@
 import React from "react"
+import "@reach/dialog/styles.css"
+import { Dialog } from "@reach/dialog"
 
 interface ButtonProps {
   text: string
+  type?: string
   onClick: (params: any) => any
 }
 
-const Button = ({ onClick, text }: ButtonProps) => {
+const Button = ({ onClick, text, type }: ButtonProps) => {
   return (
-    <button onClick={onClick} className={text === "Login" ? "purple" : "white"}>
+    <button
+      onClick={onClick}
+      type={type === "submit" ? "submit" : "button"}
+      className={text === "Login" ? "purple" : "white"}
+    >
       {text}
     </button>
   )
