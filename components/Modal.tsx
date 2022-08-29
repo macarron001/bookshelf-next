331933310ba3e6547a0440828f93375d240a6f74
Ronny <91857581+macarron001@users.noreply.github.com>
2022-08-29 14:57:07 +0800

    created modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import {
+  ModalContainer,
+  ModalContent,
+  CloseContainer,
+  CircleButton,
+  FormTitle,
+} from "./styled"
+import LoginForm from "./LoginForm"
+import RegisterForm from "./RegisterForm"
+
+interface ModalProps {
+  modalContent: string | undefined
+  setIsModalOpen: (params: boolean) => any
+}
+const Modal = ({ modalContent, setIsModalOpen }: ModalProps) => {
+  const handleClose = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <ModalContainer>
+      <ModalContent>
+        <CloseContainer>
+          <CircleButton onClick={handleClose}>x</CircleButton>
+        </CloseContainer>
+        <FormTitle>{modalContent}</FormTitle>
+        {modalContent === "Login" ? <LoginForm /> : <RegisterForm />}
+      </ModalContent>
+    </ModalContainer>
+  )
+}
+
+export default Modal
