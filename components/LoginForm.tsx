2bf8fef90bfe4225bbd0f13d842e3d0b4f15bb35
Ronny <91857581+macarron001@users.noreply.github.com>
2022-08-29 16:06:21 +0800

    added formik and yup to both forms

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -5,39 +5,58 @@ import {
   FormGroup,
   Label,
   Button,
+  Error,
 } from "./styled"
+import { useFormik } from "formik"
+import * as Yup from "yup"
 
 const LoginForm = () => {
-  const handleSubmit = (event: any) => {
-    event.preventDefault()
-    const { username, password } = event.target.elements
-
-    // onSubmit({
-    //   username: username.value,
-    //   password: password.value,
-    // })
-  }
-
-  const handleLogin = () => {
-    console.log("Logged successfully")
-  }
-
-  const handleRegister = () => {
-    console.log("Registered successfully")
-  }
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required("Required"),
+      password: Yup.string()
+        .min(6, "Must have atleast 6 characters")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
 
   return (
-    <AuthenticationForm onSubmit={handleSubmit}>
+    <AuthenticationForm>
       <FormGroup>
         <Label>Username</Label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.username && formik.errors.username ? (
+          <Error>{formik.errors.username}</Error>
+        ) : null}
       </FormGroup>
-      <FormGroup className="form-group">
+      <FormGroup>
         <Label>Password</Label>
-        <input id="username" type="text" />
+        <input
+          id="password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <Error>{formik.errors.password}</Error>
+        ) : null}
       </FormGroup>
       <FormBtnContainer>
-        <Button $purple={true} onClick={handleLogin} type="submit">
+        <Button $purple={true} onClick={formik.handleSubmit} type="submit">
           Login
         </Button>
       </FormBtnContainer>
