3fcb94ca0c9c2ea43c0a551309588551307528fb
Ronny <91857581+macarron001@users.noreply.github.com>
2022-08-29 14:57:17 +0800

    refactored index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,44 @@
-import Dialog from "@reach/dialog"
 import type { NextPage } from "next"
-import Head from "next/head"
 import Image from "next/image"
 import { useState } from "react"
-import LoginForm from "../components/LoginForm"
-import styles from "../styles/Home.module.css"
-import Button from "../utils/Button"
-import tw from "tailwind-styled-components"
+import Modal from "../components/Modal"
+import {
+  ButtonContainer,
+  IndexContainer,
+  Button,
+  Title,
+} from "../components/styled"
 
 const Home: NextPage = () => {
-  const [openModal, setOpenModal] = useState("none")
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [modalContent, setModalContent] = useState<"Login" | "Register">()
 
   const handleLogin = () => {
-    setOpenModal("login")
+    setIsModalOpen(true)
+    setModalContent("Login")
   }
 
   const handleRegister = () => {
-    setOpenModal("register")
-  }
-
-  const handleClose = () => {
-    setOpenModal("none")
-  }
-
-  const login = (formData) => {
-    console.log("login", formData)
-  }
-
-  const register = (formData) => {
-    console.log("register", formData)
+    setIsModalOpen(true)
+    setModalContent("Register")
   }
 
   return (
-    <div className={"index-container"}>
-      <Image src={"/books.png"} alt="books" width={80} height={80} />
-      <h1>Bookshelf</h1>
-      <div className="button-container">
-        <Button onClick={handleLogin} text="Login" />
-        <Button onClick={handleRegister} text="Register" />
-      </div>
-      <Dialog
-        aria-label="Login form"
-        className="dialog"
-        isOpen={openModal === "login"}
-      >
-        <div>
-          <div className="close-button">
-            <button className="circle" onClick={handleClose}>
-              x
-            </button>
-          </div>
-          <h3>Login</h3>
-          <LoginForm onSubmit={login} buttonText="Login" />
-        </div>
-      </Dialog>
-      <Dialog
-        aria-label="Registration form"
-        className="dialog"
-        isOpen={openModal === "register"}
-      >
-        <div>
-          <div className="close-button">
-            <button className="circle" onClick={handleClose}>
-              x
-            </button>
-          </div>
-          <h3>Register</h3>
-          <LoginForm onSubmit={register} buttonText="Register" />
-        </div>
-      </Dialog>
-    </div>
+    <>
+      <IndexContainer>
+        <Image src={"/books.png"} alt="books" width={80} height={80} />
+        <Title>Bookshelf</Title>
+        <ButtonContainer>
+          <Button $purple={true} onClick={handleLogin}>
+            Login
+          </Button>
+          <Button onClick={handleRegister}>Register</Button>
+        </ButtonContainer>
+      </IndexContainer>
+      {isModalOpen && (
+        <Modal modalContent={modalContent} setIsModalOpen={setIsModalOpen} />
+      )}
+    </>
   )
 }
 
