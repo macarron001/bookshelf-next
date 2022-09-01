e182afdec952a83a61e649353801b8394a90a1bf
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-01 17:37:35 +0800

    created discover component

---
+++ components/bookshelf/discover/Discover.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import SearchBox from "./SearchBox"
+import Header from "./Header"
+
+const Discover = () => {
+  return (
+    <>
+      <SearchBox />
+      <Header />
+    </>
+  )
+}
+
+export default Discover
