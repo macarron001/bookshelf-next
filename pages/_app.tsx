3fb6ebd02d9303fe6072e42b14825d5ef283fca5
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-01 23:30:01 +0800

    added userContext wrapper to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { UserContext } from "../context/UserContext"
+import { useMemo, useState } from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null)
+  const value = useMemo(() => ({ user, setUser }), [user, setUser])
+
+  return (
+    <UserContext.Provider value={value}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp
