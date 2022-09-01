0f3eea487dca42e2e46c4a671243fd76b3f9a04c
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-01 23:30:42 +0800

    updated bookshelf page

--- pages/bookshelf.tsx
+++ pages/bookshelf.tsx
@@ -2,26 +2,17 @@ import { NextPage } from "next"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import Navigation from "../components/bookshelf/Navigation"
-import SearchBox from "../components/bookshelf/discover/SearchBox"
-import { Button } from "../components/styled"
-import {
-  Container,
-  HeaderBox,
-  LogoutContainer,
-  HeaderItem,
-  Name,
-} from "../components/styled/bookshelf"
-import { useState } from "react"
+import { Container } from "../components/styled/bookshelf"
+import { useState, useContext } from "react"
 import Discover from "../components/bookshelf/discover/Discover"
+import LogoutBox from "../components/bookshelf/LogoutBox"
 
 const Home: NextPage = () => {
   const [active, setActive] = useState("Reading List")
+
   return (
     <>
-      <LogoutContainer>
-        <Name>rpinoon</Name>
-        <Button>Logout</Button>
-      </LogoutContainer>
+      <LogoutBox />
       <Container>
         <Navigation active={active} setActive={setActive} />
         {active === "Discover" && <Discover />}
