8c0db40501ee08ca6e9bdb186b285232c040af59
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-02 16:44:09 +0800

    check if user is logged-in, persist user and books state context

--- pages/bookshelf.tsx
+++ pages/bookshelf.tsx
@@ -1,23 +1,61 @@
 import { NextPage } from "next"
-import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import Navigation from "../components/bookshelf/Navigation"
 import { Container } from "../components/styled/bookshelf"
-import { useState, useContext } from "react"
+import { useState, useContext, useEffect, useMemo } from "react"
 import Discover from "../components/bookshelf/discover/Discover"
 import LogoutBox from "../components/bookshelf/LogoutBox"
+import { UserContext } from "../context/UserContext"
+import { useRouter } from "next/router"
+import { getBooks } from "../api/books"
+import { BooksContext } from "../context/BooksContext"
 
 const Home: NextPage = () => {
   const [active, setActive] = useState("Reading List")
+  const { user, setUser } = useContext(UserContext)
+  const [books, setBooks] = useState(null)
+  const value = useMemo(() => ({ books, setBooks }), [books, setBooks])
+  const router = useRouter()
 
+  useEffect(() => {
+    const existingUser = JSON.parse(localStorage.getItem("user"))
+    if (existingUser) {
+      setUser(existingUser)
+    } else {
+      router.push("/")
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    const existingBooks = JSON.parse(localStorage.getItem("books"))
+    if (existingBooks) {
+      setBooks(existingBooks)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      if (user) {
+        const bookList = await getBooks(user)
+        console.log(bookList)
+        setBooks(bookList)
+      }
+    }
+
+    fetchBooks().catch(console.error)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <>
-      <LogoutBox />
-      <Container>
-        <Navigation active={active} setActive={setActive} />
-        {active === "Discover" && <Discover />}
-      </Container>
-      <ToastContainer />
+      <BooksContext.Provider value={value}>
+        <LogoutBox />
+        <Container>
+          <Navigation active={active} setActive={setActive} />
+          {active === "Discover" && <Discover />}
+        </Container>
+      </BooksContext.Provider>
     </>
   )
 }
