34da9a67485af4ccec5afe1d0472134ffcc82ef9
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-02 16:41:57 +0800

    added notification container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { UserContext } from "../context/UserContext"
 import { useMemo, useState } from "react"
+import { ToastContainer } from "react-toastify"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState(null)
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserContext.Provider value={value}>
       <Component {...pageProps} />
+      <ToastContainer />
     </UserContext.Provider>
   )
 }
