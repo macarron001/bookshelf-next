5848e6d85178fe59d4efe5ae9c12aec615fae4e9
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-05 12:18:42 +0800

    created book component

--- components/bookshelf/Book.tsx
+++ components/bookshelf/Book.tsx
@@ -1,7 +1,49 @@
 import React from "react"
+import Image from "next/image"
+import {
+  BookCard,
+  BookContent,
+  BookImage,
+  HeaderBar,
+  InfoBox,
+  Author,
+  Publisher,
+  Title,
+  Synopsis,
+  SideBar,
+  PlusButton,
+  RoundBorder,
+} from "../styled/bookshelf"
 
-const Book = () => {
-  return <div></div>
+const Book = ({ book }) => {
+  return (
+    <>
+      <BookCard>
+        <BookImage>
+          <Image
+            src={`${book.cover_image_url}`}
+            alt=""
+            width={100}
+            height={150}
+          />
+        </BookImage>
+        <BookContent>
+          <HeaderBar>
+            <Title>{book.title}</Title>
+            <InfoBox>
+              <Author>{book.author}</Author>
+              <Publisher>{book.publisher}</Publisher>
+            </InfoBox>
+          </HeaderBar>
+          <Synopsis>{book.synopsis}</Synopsis>
+        </BookContent>
+        <SideBar>
+          <PlusButton>➕</PlusButton>
+          <RoundBorder />
+        </SideBar>
+      </BookCard>
+    </>
+  )
 }
 
 export default Book
