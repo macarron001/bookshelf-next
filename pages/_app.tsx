4c881d972a85727df6528cb2bd17a5d5ad23f30c
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-05 12:19:26 +0800

    included books context wrapper in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,20 @@ import type { AppProps } from "next/app"
 import { UserContext } from "../context/UserContext"
 import { useMemo, useState } from "react"
 import { ToastContainer } from "react-toastify"
+import { BooksContext } from "../context/BooksContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState(null)
+  const [books, setBooks] = useState(null)
   const value = useMemo(() => ({ user, setUser }), [user, setUser])
+  const bookValue = useMemo(() => ({ books, setBooks }), [books, setBooks])
 
   return (
     <UserContext.Provider value={value}>
-      <Component {...pageProps} />
-      <ToastContainer />
+      <BooksContext.Provider value={bookValue}>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </BooksContext.Provider>
     </UserContext.Provider>
   )
 }
