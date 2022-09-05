8dadeaa08bcdb4ddcd9d5477b545abf4ea3c19c4
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-05 13:09:39 +0800

    render different buttons when adding book to reading list

--- components/bookshelf/Book.tsx
+++ components/bookshelf/Book.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
 import {
   BookCard,
@@ -11,11 +11,29 @@ import {
   Title,
   Synopsis,
   SideBar,
-  PlusButton,
-  RoundBorder,
+  ExtendedSideBar,
 } from "../styled/bookshelf"
 
 const Book = ({ book }) => {
+  const [isAdding, setIsAdding] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const addToList = () => {
+    setIsAdding(true)
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2500)
+  }
+
+  const markAsRead = () => {
+    console.log("to read")
+  }
+
+  const removeFromList = () => {
+    console.log("remove from list")
+  }
+
   return (
     <>
       <BookCard>
@@ -38,8 +56,26 @@ const Book = ({ book }) => {
           <Synopsis>{book.synopsis}</Synopsis>
         </BookContent>
         <SideBar>
-          <PlusButton>➕</PlusButton>
-          <RoundBorder />
+          {!isAdding && !isLoading && (
+            <button onClick={addToList}>
+              <Image src={"/plus.png"} alt="" width={20} height={20} />
+            </button>
+          )}
+          {isLoading && (
+            <Image
+              className="bg-transparent opacity-30"
+              src={"/spinner.gif"}
+              alt=""
+              width={20}
+              height={20}
+            />
+          )}
+          {isAdding && !isLoading && (
+            <ExtendedSideBar>
+              <button onClick={markAsRead}>✅</button>
+              <button onClick={removeFromList}>⛔</button>
+            </ExtendedSideBar>
+          )}
         </SideBar>
       </BookCard>
     </>
