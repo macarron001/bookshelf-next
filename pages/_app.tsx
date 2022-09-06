63e936ad5289ad548b438d55ae5ae3890d0d2cc0
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-06 16:19:08 +0800

    cleaned up _app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,16 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { UserProvider } from "../context/UserContext"
-import { useMemo, useState } from "react"
+import { BooksProvider } from "../context/BooksContext"
 import { ToastContainer } from "react-toastify"
-import { BooksContext } from "../context/BooksContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [books, setBooks] = useState(null)
-  const bookValue = useMemo(() => ({ books, setBooks }), [books, setBooks])
-
   return (
     <UserProvider>
-      <BooksContext.Provider value={bookValue}>
+      <BooksProvider>
         <Component {...pageProps} />
         <ToastContainer />
-      </BooksContext.Provider>
+      </BooksProvider>
     </UserProvider>
   )
 }
