c6b65bbd596ebbc9676d21ae6b72a7b7c1147dd7
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-06 14:50:34 +0800

    changed file to tsx and updated userContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,21 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import { UserContext } from "../context/UserContext"
+import { UserProvider } from "../context/UserContext"
 import { useMemo, useState } from "react"
 import { ToastContainer } from "react-toastify"
 import { BooksContext } from "../context/BooksContext"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [user, setUser] = useState(null)
   const [books, setBooks] = useState(null)
-  const value = useMemo(() => ({ user, setUser }), [user, setUser])
   const bookValue = useMemo(() => ({ books, setBooks }), [books, setBooks])
 
   return (
-    <UserContext.Provider value={value}>
+    <UserProvider>
       <BooksContext.Provider value={bookValue}>
         <Component {...pageProps} />
         <ToastContainer />
       </BooksContext.Provider>
-    </UserContext.Provider>
+    </UserProvider>
   )
 }
 
