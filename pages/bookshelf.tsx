aa6fcc21567388021124f370a4264fc9910251f9
Ronny <91857581+macarron001@users.noreply.github.com>
2022-09-07 09:59:37 +0800

    integrated authorized layout in bookshelf page

--- pages/bookshelf.tsx
+++ pages/bookshelf.tsx
@@ -2,29 +2,19 @@ import { NextPage } from "next"
 import "react-toastify/dist/ReactToastify.css"
 import Navigation from "../components/bookshelf/Navigation"
 import { Container } from "../components/styled/bookshelf"
-import { useState, useContext, useEffect, useMemo } from "react"
+import { useState, useContext, useEffect } from "react"
 import Discover from "../components/bookshelf/discover/Discover"
 import LogoutBox from "../components/bookshelf/LogoutBox"
 import { UserContext } from "../context/UserContext"
-import { useRouter } from "next/router"
 import { getBooks } from "../api/books"
 import { BooksContext } from "../context/BooksContext"
 import { ActivePageEnum } from "../api/enums"
+import AuthorizedLayout from "../components/AuthorizedLayout"
 
 const Home: NextPage = () => {
   const [active, setActive] = useState<ActivePageEnum | string>(
     ActivePageEnum.to_read
   )
-  const router = useRouter()
-
-  useEffect(() => {
-    const existingUser = JSON.parse(localStorage.getItem("user") || "null")
-    existingUser ? setUser(existingUser) : router.push("/")
-
-    const existingBooks = JSON.parse(localStorage.getItem("books") || "null")
-    if (existingBooks) setBooks(existingBooks)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -44,7 +34,7 @@ const Home: NextPage = () => {
   if (!contextUser) {
     return null
   }
-  const { user, setUser } = contextUser
+  const { user } = contextUser
 
   if (!contextBooks) {
     return null
@@ -53,11 +43,13 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <LogoutBox />
-      <Container>
-        <Navigation active={active} setActive={setActive} />
-        {active === ActivePageEnum.discover && <Discover />}
-      </Container>
+      <AuthorizedLayout>
+        <LogoutBox />
+        <Container>
+          <Navigation active={active} setActive={setActive} />
+          {active === ActivePageEnum.discover && <Discover />}
+        </Container>
+      </AuthorizedLayout>
     </>
   )
 }
